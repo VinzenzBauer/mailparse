6db01e8799d14571f9abad91ab183c55b069a259
vinzenzbauer <vinzenz.bauer@gmx.de>
2024-03-22 19:34:33 +0100

    work work

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ perl -MCPAN -e shell
 cpan[1]>  o conf prerequisites_policy follow
 cpan[2]>  o conf commit
 exit
+
+# execute from git providing the mail mail_unformated10
+wget -O - https://raw.githubusercontent.com/VinzenzBauer/mailparse/main/parse_mail.pl $1 2>&1 | cat mail_unformated10 | perl ./parse_mail.p
---
+++ test.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use HTML::Strip;
+
+### VARS
+my $M0 = "named_attribute: sasl_username=";
+my $DATE = "Date:";
+my $FROM = "From:";
+my $TO = "To:";
+my $SUBJECT = "Subject:";
+
+my $bodystart="<body";
+my $bodyend = "\/body>";
+my $body = "";
+my @removes = qw(&nbsp; &copy; \t);
+
+my $stuff_path = '/home/vinzenz/mailparse/mail_unformated';
+my $PIPE = "";
+my $CTYPE = 0;
+
+### FUNCTIONS
+sub remove_strings {
+	my ($input, $rem) = @_;
+	my @rem = @{ $rem };
+	
+	foreach my $remove (@rem){
+		$input =~ s/$remove//g;
+	}
+	return $input;
+}
+
+### MAIN
+foreach my $line ( <STDIN> ) {
+    chomp( $line );
+	$PIPE .= $line;
+
+	### MAIL HEADER
+	if ($line =~ m/^$M0/) {
+		#named_attribute: sasl_username=m05ad8d7
+		$line =~ s/^[^=]*=//;
+  		printf "%s:\t%s\n", "Postfach", $line;
+	};
+	if ($line =~ m/^$DATE/) {
+		#Date: Wed, 20 Mar 2024 16:31:40 +0000 (UTC)
+		$line =~ s/^[^,]*\s,//;
+  		printf "%s:\t\t%s\n", "Am", $line;
+	};
+	if ($line =~ m/^$FROM/) {
+		#From: TPMS-SAS-Live <tpms-sas-live@auto.prodefis-outbox.de>
+		$line =~ s/^[^:]*:\s//;
+  		printf "%s:\t\t%s\n", "Von", $line;
+	};
+	if ($line =~ m/^$TO/) {
+		#To: "Messing Ragert, Ingrid  10162" <10162@sas.dk>
+		$line =~ s/^[^:]*:\s//;
+  		printf "%s:\t\t%s\n", "An", $line;
+	};
+	if ($line =~ m/^$SUBJECT/) {
+		#Subject: Event report Filed
+		$line =~ s/^[^:]*:\s//;
+  		printf "%s:\t%s\n", "Betreff", $line;
+	};
+	
+	### MAIL CONTENT
+	if ($line =~ m/^Content-Type: text\/html;/) {
+		$CTYPE = 1
+	}
+	if ($line =~ m/^Content-Type: text\/plain;/) {
+		$CTYPE = 2
+	}
+	
+	if ($line =~ m/^.*$bodyend/) {
+		$PIPE =~ s/.*$bodystart/$bodystart.*/;		# alles bis <body raus
+		$PIPE =~ s/$bodyend.*/.*$bodyend/;			# alles nach /body> raus
+		$PIPE = remove_strings($PIPE, \@removes);	# spezielle tags raus
+		$PIPE =~ s|<.+?>||g;						# html raus
+		#$PIPE =~ s|{.+?}||g;						# webkit raus
+		$PIPE =~ s/{.+}//sg;
+		$PIPE =~ s/\h+/ /g;							# remove multispaces
+		
+		#my $hs = HTML::Strip->new();
+		#my $clean_text = $hs->parse( $PIPE );
+		#$hs->eof;
+
+  		printf "%s:\t%s\n", "Content", "$PIPE";
+	};
+}
+
+#my $hs = HTML::Strip->new();
+#my $clean_text = $hs->parse( $PIPE );
+#$hs->eof;
+#printf "%s:\t%s\n", "Content", "$clean_text";
+		
+
+if ($CTYPE == 2){
+	$PIPE =~ s/.*X-Spamd-Bar://;				# alles bis raus
+	$PIPE =~ s/\*\*\*.*/\*\*\*/;				# alles nach raus
+	$PIPE = remove_strings($PIPE, \@removes);	# spezielle tags raus
+	$PIPE =~ s/\h+/ /g;							# replace multispaces with single space
+	printf "%s:\t%s\n", "Content", "$PIPE";
+}
+
+### END
+1
