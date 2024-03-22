717a7a14a6933d3e89d3ccac93fa6970a99f4ca2
vinzenzbauer <vinzenz.bauer@gmx.de>
2024-03-22 23:18:38 +0100

    base64

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ exit
 
 # execute from git providing the mail mail_unformated10
 cat mail_unformated10 | perl <(wget -qO- https://raw.githubusercontent.com/VinzenzBauer/mailparse/main/parse_mail.pl)
+
+# execute local with parser existing
+./parse_mail.pl <mail_unformated10
--- parse_mail.pl
+++ parse_mail.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use HTML::Strip;
+use MIME::Base64;
 
 ### VARS
 my $M0 = "named_attribute: sasl_username=";
@@ -18,6 +19,8 @@ my @removes = qw(&nbsp; &copy; \t);
 my $stuff_path = '/home/vinzenz/mailparse/mail_unformated';
 my $PIPE = "";
 my $CTYPE = 0;
+my $BASE64 = 0;
+my $dup = "";
 
 ### FUNCTIONS
 sub remove_strings {
@@ -62,43 +65,82 @@ foreach my $line ( <STDIN> ) {
   		printf "%s:\t%s\n", "Betreff", $line;
 	};
 	
+	## BASE64 DECODE
+	if ($line =~ m/^Content-Transfer-Encoding: base64/) {
+		$BASE64 = 1;
+	}
+
 	### MAIL CONTENT
 	if ($line =~ m/^Content-Type: text\/html;/) {
-		$CTYPE = 1
+		$CTYPE = 1;
 	}
 	if ($line =~ m/^Content-Type: text\/plain;/) {
-		$CTYPE = 2
+		$CTYPE = 2;
 	}
 	
+	### HTML CONTENT
 	if ($line =~ m/^.*$bodyend/) {
-		$PIPE =~ s/.*$bodystart/$bodystart.*/;		# alles bis <body raus
-		$PIPE =~ s/$bodyend.*/.*$bodyend/;			# alles nach /body> raus
-		$PIPE = remove_strings($PIPE, \@removes);	# spezielle tags raus
-		$PIPE =~ s|<.+?>||g;						# html raus
-		#$PIPE =~ s|{.+?}||g;						# webkit raus
-		$PIPE =~ s/{.+}//sg;
-		$PIPE =~ s/\h+/ /g;							# remove multispaces
+		$dup = $PIPE;
+		#$dup =~ s/.*$bodystart/$bodystart.*/;		# alles bis <body raus
+		#$dup =~ s/$bodyend.*/.*$bodyend/;			# alles nach /body> raus
+		$dup =~ s/.*$bodystart>(.*)$bodyend/$1/;	# zwischen start und end
+		$dup = remove_strings($dup, \@removes);		# spezielle tags raus
+		$dup =~ s|<.+?>||g;							# html raus
+		#$dup =~ s|{.+?}||g;						# webkit raus
+		$dup =~ s/{.+}//sg;							# webkit raus
+		$dup =~ s/\h+/ /g;							# remove multispaces
 		
 		#my $hs = HTML::Strip->new();
-		#my $clean_text = $hs->parse( $PIPE );
+		#my $clean_text = $hs->parse( $dup );
 		#$hs->eof;
 
-  		printf "%s:\t%s\n", "Content", "$PIPE";
+  		printf "%s:\t%s\n", "Content", "$dup";
 	};
 }
 
-#my $hs = HTML::Strip->new();
-#my $clean_text = $hs->parse( $PIPE );
-#$hs->eof;
-#printf "%s:\t%s\n", "Content", "$clean_text";
-		
-
+### PLAIN TEXT CONTENT
 if ($CTYPE == 2){
-	$PIPE =~ s/.*X-Spamd-Bar://;				# alles bis raus
-	$PIPE =~ s/\*\*\*.*/\*\*\*/;				# alles nach raus
-	$PIPE = remove_strings($PIPE, \@removes);	# spezielle tags raus
-	$PIPE =~ s/\h+/ /g;							# replace multispaces with single space
-	printf "%s:\t%s\n", "Content", "$PIPE";
+	$dup = $PIPE;
+	$dup =~ s/.*X-Spamd-Bar: \/(.*)/$1/;			# alles bis raus
+	$dup =~ s/\*\*\*.*\*\*\*//;						# alles nach raus
+	$dup = remove_strings($dup, \@removes);			# spezielle tags raus
+	$dup =~ s/\h+/ /g;								# replace multispaces with single space
+	printf "%s:\t%s\n", "Content", "$dup";
+}
+
+
+### BASE64 TEXT CONTENTS
+if ($BASE64 == 1){
+	$dup = $PIPE;
+	$dup =~ s/.*Content-Type: text\/plain.*?Content-Transfer-Encoding: base64(.*?)=.*/$1/;
+	#printf "%s:\t%s\n", "Base64", "$dup";
+	
+	my $decoded = MIME::Base64::decode($dup);
+	printf "%s:\t\t%s\n", "Base64", "$decoded";
+
+	#Content-Type: text/plain; charset="UTF-8"; format=flowed; delsp=yes
+	#Content-Transfer-Encoding: base64
+
+	#PT09IE5ld3MgLSAxIG5ldWVzIEVyZ2VibmlzIGbDvHIgW2VpZ2VuZXIgbmV3c2xldHRlcl0gPT09
+	#DQoNCkV4LVJBRi1UZXJyb3Jpc3RpbiBLbGV0dGUgc29sbCAxNDAuMDAwIEV1cm8gaW4gZWlnZW5l
+	#ciBXb2hudW5nIHZlcnN0ZWNrdA0KaGFiZW4gLSBSTkQNClJORA0KTWl0IG1laW5lciBBbm1lbGR1
+	#bmcgenVtIE5ld3NsZXR0ZXIgc3RpbW1lIGljaCBkZXIgLi4uDQo8aHR0cHM6Ly93d3cuZ29vZ2xl
+	#LmNvbS91cmw/cmN0PWomc2E9dCZ1cmw9aHR0cHM6Ly93d3cucm5kLmRlL3BvbGl0aWsvcmFmLWts
+	#ZXR0ZS1zb2xsLTE0MC0wMDAtZXVyby1pbi1laWdlbmVyLXdvaG51bmctdmVyc3RlY2t0LWhhYmVu
+	#LU9PWUNRWlhYNFZFUTNLS0xGSlM0NlpQNE1ZLmh0bWwmY3Q9Z2EmY2Q9Q0FFWUFDb1VNVFkzTkRJ
+	#NE16RTNOek0xTWpZM05EUXlORGd5R2preFpqazVPRFkzWXpJd01UQTJPV0k2WTI5dE9tUmxPbFZU
+	#JnVzZz1BT3ZWYXcxOWZVSkZpOWROOG1BTFliUWZORDlCPg0KDQoNCi0gLSAtIC0gLSAtIC0gLSAt
+	#IC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0NCkRpZXNl
+	#biBHb29nbGUgQWxlcnQgbmljaHQgbWVociBlcmhhbHRlbjoNCjxodHRwczovL3d3dy5nb29nbGUu
+	#Y29tL2FsZXJ0cy9yZW1vdmU/c291cmNlPWFsZXJ0c21haWwmaGw9ZGUmZ2w9VVMmbXNnaWQ9TVRZ
+	#M05ESTRNekUzTnpNMU1qWTNORFF5TkRnJnM9QUIyWHE0Z05KekpmZlJ4bFZwNHR3OVpIRkVSUzhZ
+	#M3N3QjlnSF9jPg0KDQpXZWl0ZXJlbiBHb29nbGUgQWxlcnQgZXJzdGVsbGVuOg0KPGh0dHBzOi8v
+	#d3d3Lmdvb2dsZS5jb20vYWxlcnRzP3NvdXJjZT1hbGVydHNtYWlsJmhsPWRlJmdsPVVTJm1zZ2lk
+	#PU1UWTNOREk0TXpFM056TTFNalkzTkRReU5EZz4NCg0KTWVsZGUgZGljaCBhbiwgdW0gZGVpbmUg
+	#QWxlcnRzIHp1IHZlcndhbHRlbjoNCjxodHRwczovL3d3dy5nb29nbGUuY29tL2FsZXJ0cz9zb3Vy
+	#Y2U9YWxlcnRzbWFpbCZobD1kZSZnbD1VUyZtc2dpZD1NVFkzTkRJNE16RTNOek0xTWpZM05EUXlO
+	#RGc+DQo=
+	#--000000000000ab91de0614407291
 }
 
 ### END
