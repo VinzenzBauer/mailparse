9a573d930ac774bd0a840e9af0b05080785feb8d
vinzenzbauer <vinzenz.bauer@gmx.de>
2024-03-24 21:29:38 +0100

    more work

--- parse_mail.pl
+++ parse_mail.pl
@@ -2,137 +2,184 @@
 use strict;
 use warnings;
 use MIME::Base64;
+use Term::ANSIColor;
 
 ### VARS
-my $M0 = "named_attribute: sasl_username=";				#named_attribute: sasl_username=m05ad8d7
-my $IP = "named_attribute: client_address=";			#client_address=209.85.128.197
-my $DATE = "Date:.+[0-9]{2}:[0-9]{2}:[0-9]{2}.+/";		#Date: Wed, 20 Mar 2024 16:31:40 +0000 (UTC)
-my $FROM = "From:";										#From: TPMS-SAS-Live <tpms-sas-live@auto.prodefis-outbox.de>
-my $TO = "To:";											#To: "Messing Ragert, Ingrid  10162" <10162@sas.dk>
-my $SUBJECT = "Subject:";								#Subject: Event report Filed
-
-my $bodystart="<body>";
-my $bodyend = "\/body>";
-my $body = "";
-my @removes = qw(&nbsp; &copy; \t);
+my $M0 = "named_attribute: sasl_username=";         #named_attribute: sasl_username=m05ad8d7
+my $IP = "named_attribute: client_address=";        #client_address=209.85.128.197
+my $DATE = "Date:.*[0-9]{2}:[0-9]{2}:[0-9]{2}";     #Date: Wed, 20 Mar 2024 16:31:40 +0000 (UTC)
+my $FROM = "From:";                                	#From: TPMS-SAS-Live <tpms-sas-live@auto.prodefis-outbox.de>
+my $TO = "To:";                                     #To: "Messing Ragert, Ingrid  10162" <10162@sas.dk>
+my $SUBJECT = "Subject:";                           #Subject: Event report Filed
+my $REPLYTO = "Reply-To:";
+						
+my $htmlstart="<[t]?body.*?>";						#<body class="mainBody"> #<tbody>
+my $htmlend = "<.*?\/[t]?body>";					#hmtl tag not exist in 3
+my @removes = qw/ &nbsp; &copy; \t =09 &quot; = &zwnj; 0A /;			# bsp: 1
+my @removes_iso = qw/ =\?iso-8859-1\?B\? =\?= \?= /;		# bsp: 5
 
 my $PIPE = "";
-my $CTYPE = 0;
-my $BASE64 = 0;
-my $dup = "";
+my $base64 = "";
+my $content = "";
+
+my $bounce = 0;
 
 ### FUNCTIONS
 sub remove_strings {
-	my ($input, $rem) = @_;
-	my @rem = @{ $rem };
-	
-	foreach my $remove (@rem){
-		$input =~ s/$remove//g;
-	}
-	return $input;
+    my ($input, $rem) = @_;
+    my @rem = @{ $rem };
+    
+    foreach my $remove (@rem){
+        $input =~ s/$remove//g;
+    }
+    return $input;
+}
+sub decode_iso_8859_1_base64 {		# bsp 5: subject from etc waren base64 mit iso-tags zb: =?iso-8859-1?B?Kysg
+	my $encoded = shift;
+	$encoded = remove_strings($encoded, \@removes_iso);
+	my $decoded = MIME::Base64::decode($encoded);
+    return $decoded;
+}
+sub decode_guess{
+	my $line = shift;
+	if ($line =~ m/=\?iso/) {
+		return decode_iso_8859_1_base64("$line");
+	};
+	return $line;
 }
 
-### MAIN
+### READ WHOLE FILE
 foreach my $line ( <STDIN> ) {
-    chomp( $line );
-	$PIPE .= $line;
+    #chomp( $line );
+    $PIPE .= $line;
+}
 
-	### MAIL HEAD
+### SEPARATE HEAD AND BODY
+my ($head, $body) = split /\n\h*\n/, $PIPE, 2;
+
+### MAIL HEAD
+#print color("red"), "$head\n", color("reset");
+my @headA = split (/\n/, $head);
+foreach my $line (@headA) {
+	#print "$line\n";
 	if ($line =~ m/^$M0/) {
 		$line =~ s/^[^=]*=//;
-  		printf "%s:\t%s\n", "Postfach", $line;
+		printf "%s:\t\t%s ", "sasl", $line;
 	};
 	if ($line =~ m/$IP/) {
 		$line =~ s/^[^=]*=//;
-  		printf "%s:\t\t%s\n", "Ip", $line;
+		printf "<%s>\n", $line;
 	};
 	if ($line =~ m/^$DATE/) {
-		$line =~ s/^[^,]*\s,//;
-  		printf "%s:\t\t%s\n", "Am", $line;
+		$line =~ s/^[^,]*,\s//;
+		printf "%s:\t\t%s\n", "date", $line;
 	};
 	if ($line =~ m/^$FROM/) {
 		$line =~ s/^[^:]*:\s//;
-  		printf "%s:\t\t%s\n", "Von", $line;
+		$line = decode_guess("$line");
+		printf "%s:\t\t%s\n", "from", $line;
 	};
 	if ($line =~ m/^$TO/) {
 		$line =~ s/^[^:]*:\s//;
-  		printf "%s:\t\t%s\n", "An", $line;
+		$line = decode_guess("$line");
+		printf "%s:\t\t%s\n", "to", $line;
+	};
+	if ($line =~ m/^$REPLYTO/) {
+		$line =~ s/^[^:]*:\s//;
+		$line = decode_guess("$line");
+		printf "%s:\t%s\n", "replyto", $line;
 	};
 	if ($line =~ m/^$SUBJECT/) {
 		$line =~ s/^[^:]*:\s//;
-  		printf "%s:\t%s\n", "Betreff", $line;
-	};
-	
-	## BASE64 DECODE
-	if ($line =~ m/^Content-Transfer-Encoding: base64/) {
-		$BASE64 = 1;
-	}
-
-	### MAIL CONTENT
-	if ($line =~ m/^Content-Type: text\/html;/) {
-		$CTYPE = 1;
-	}
-	if ($line =~ m/^Content-Type: text\/plain;/) {
-		$CTYPE = 2;
-	}
-	
-	### HTML CONTENT
-	if ($line =~ m/^.*$bodyend/) {
-		$dup = $PIPE;
-		$dup =~ s/.*$bodystart(.*)$bodyend/$1/;		# zwischen start und end
-		$dup = remove_strings($dup, \@removes);		# spezielle tags raus
-		$dup =~ s|<.+?>||g;							# html raus
-		#$dup =~ s|{.+?}||g;						# webkit raus
-		$dup =~ s/{.+}//sg;							# webkit raus
-		$dup =~ s/\h+/ /g;							# remove multispaces
-
-  		printf "%s:\t%s\n", "Content", "$dup";
+		$line = decode_guess("$line");
+		printf "%s:\t%s\n", "subject", $line;
 	};
 }
 
-### PLAIN TEXT CONTENT
-if ($CTYPE == 2){
-	$dup = $PIPE;
-	$dup =~ s/.*X-Spamd-Bar: \/(.*)/$1/;			# alles bis raus
-	$dup =~ s/\*\*\*.*\*\*\*//;						# alles nach raus
-	$dup = remove_strings($dup, \@removes);			# spezielle tags raus
-	$dup =~ s/\h+/ /g;								# replace multispaces with single space
-	printf "%s:\t%s\n", "Content", "$dup";
+### MAIL BODY
+#print color("green"), "$body\n", color("reset");
+
+## HTML CONTENT (bsp: 1, 3, 7)
+$content = $body; 
+$content =~ s/\R//g;							# remove linebreaks
+$content =~ s/.*?$htmlstart(.*)$htmlend.*/$1/s;	# extract content between html-body-tags
+$content =~ s|<.+?>||g;                         # html raus
+$content =~ s/{.+}//sg;                         # webkit raus
+$content = remove_strings($content, \@removes);	# spezielle tags raus
+$content =~ s/^ +//gm ;							# remove whitespaces at start of line
+$content =~ s/\n\s*/\n/g;						# remove empty lines
+$content =~ s/\h+/ /g;                        	# replace multispaces with single space
+
+## ERROR EG.: Content-Description: Undelivered Message
+#Reply-To: WinBTC <bitcoin-mining@groupalim.com>
+#From: WinBTC <benjamin@widmann-elektrotechnik.de>
+#Subject: Balance Mining BTC 
+#Date: Fri, 22 Mar 2024 16:29:30 +0000
+my @bodyA = split (/\n/, $body);
+if ($body =~ /I'm sorry to have to inform you that your message could not/s) {
+	print '-' x 80, "\n"; 
+	$bounce = 1;
+	
+	foreach my $line (@bodyA) {
+		if ($line =~ m/^$M0/) {
+			$line =~ s/^[^=]*=//;
+			printf "%s:\t\t%s ", "sasl", $line;
+		};
+		if ($line =~ m/$IP/) {
+			$line =~ s/^[^=]*=//;
+			printf "<%s>\n", $line;
+		};
+		if ($line =~ m/^$DATE/) {
+			$line =~ s/^[^,]*,\s//;
+			printf "%s:\t\t%s\n", "date", $line;
+		};
+		if ($line =~ m/^$FROM/) {
+			$line =~ s/^[^:]*:\s//;
+			printf "%s:\t\t%s\n", "from", $line;
+		};
+		if ($line =~ m/^$TO/) {
+			$line =~ s/^[^:]*:\s//;
+			printf "%s:\t\t%s\n", "to", $line;
+		};
+		if ($line =~ m/^$REPLYTO/) {
+			$line =~ s/^[^:]*:\s//;
+			printf "%s:\t%s\n", "replyto", $line;
+		};
+		if ($line =~ m/^$SUBJECT/) {
+			$line =~ s/^[^:]*:\s//;
+			printf "%s:\t%s\n", "subject", $line;
+		};
+	}
+	# bsp: 10 alles nach X-Spamd-Bar: von interesse: Hallo * * * 馃獧 袙袗袦 袟袗效袠小袥袝袧袨 ...
+	if ($body =~ /X-Spamd-Bar: \/\n/s) {
+		$content = $body;
+		$content =~ s/.*?X-Spamd-Bar: \/\n(.*?)--.*/$1/s;
+		$content =~ s/\R//g;							# remove linebreaks
+		$content =~ s|<.+?>||g;                         # html raus
+		
+		print color("green"), "$content\n", color("reset");
+	}
 }
 
+if ( !$bounce ){		# bsp 6 ist ein bounce -> nix valides zum zeigen
+	print color("green"), "$content\n", color("reset");
+}
 
-### BASE64 TEXT CONTENTS
-if ($BASE64 == 1){
-	$dup = $PIPE;
-	$dup =~ s/.*Content-Type: text\/plain.*?Content-Transfer-Encoding: base64(.*?)=.*/$1/;
-	#printf "%s:\t%s\n", "Base64", "$dup";
+### BASE64 TEXT CONTENTS			# (bsp: 6 )
+if ($body =~ /Content-Type: (text\/plain|text\/html);(?=.{5,200}Content-Transfer-Encoding: base64)/s) {
+	#print color("red"), "Base64 DECODE:\n", color("reset");
+	$base64 = $body;
 	
-	my $decoded = MIME::Base64::decode($dup);
-	printf "%s:\t\t%s\n", "Base64", "$decoded";
-
-	#Content-Type: text/plain; charset="UTF-8"; format=flowed; delsp=yes
-	#Content-Transfer-Encoding: base64
-
-	#PT09IE5ld3MgLSAxIG5ldWVzIEVyZ2VibmlzIGbDvHIgW2VpZ2VuZXIgbmV3c2xldHRlcl0gPT09
-	#DQoNCkV4LVJBRi1UZXJyb3Jpc3RpbiBLbGV0dGUgc29sbCAxNDAuMDAwIEV1cm8gaW4gZWlnZW5l
-	#ciBXb2hudW5nIHZlcnN0ZWNrdA0KaGFiZW4gLSBSTkQNClJORA0KTWl0IG1laW5lciBBbm1lbGR1
-	#bmcgenVtIE5ld3NsZXR0ZXIgc3RpbW1lIGljaCBkZXIgLi4uDQo8aHR0cHM6Ly93d3cuZ29vZ2xl
-	#LmNvbS91cmw/cmN0PWomc2E9dCZ1cmw9aHR0cHM6Ly93d3cucm5kLmRlL3BvbGl0aWsvcmFmLWts
-	#ZXR0ZS1zb2xsLTE0MC0wMDAtZXVyby1pbi1laWdlbmVyLXdvaG51bmctdmVyc3RlY2t0LWhhYmVu
-	#LU9PWUNRWlhYNFZFUTNLS0xGSlM0NlpQNE1ZLmh0bWwmY3Q9Z2EmY2Q9Q0FFWUFDb1VNVFkzTkRJ
-	#NE16RTNOek0xTWpZM05EUXlORGd5R2preFpqazVPRFkzWXpJd01UQTJPV0k2WTI5dE9tUmxPbFZU
-	#JnVzZz1BT3ZWYXcxOWZVSkZpOWROOG1BTFliUWZORDlCPg0KDQoNCi0gLSAtIC0gLSAtIC0gLSAt
-	#IC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0gLSAtIC0NCkRpZXNl
-	#biBHb29nbGUgQWxlcnQgbmljaHQgbWVociBlcmhhbHRlbjoNCjxodHRwczovL3d3dy5nb29nbGUu
-	#Y29tL2FsZXJ0cy9yZW1vdmU/c291cmNlPWFsZXJ0c21haWwmaGw9ZGUmZ2w9VVMmbXNnaWQ9TVRZ
-	#M05ESTRNekUzTnpNMU1qWTNORFF5TkRnJnM9QUIyWHE0Z05KekpmZlJ4bFZwNHR3OVpIRkVSUzhZ
-	#M3N3QjlnSF9jPg0KDQpXZWl0ZXJlbiBHb29nbGUgQWxlcnQgZXJzdGVsbGVuOg0KPGh0dHBzOi8v
-	#d3d3Lmdvb2dsZS5jb20vYWxlcnRzP3NvdXJjZT1hbGVydHNtYWlsJmhsPWRlJmdsPVVTJm1zZ2lk
-	#PU1UWTNOREk0TXpFM056TTFNalkzTkRReU5EZz4NCg0KTWVsZGUgZGljaCBhbiwgdW0gZGVpbmUg
-	#QWxlcnRzIHp1IHZlcndhbHRlbjoNCjxodHRwczovL3d3dy5nb29nbGUuY29tL2FsZXJ0cz9zb3Vy
-	#Y2U9YWxlcnRzbWFpbCZobD1kZSZnbD1VUyZtc2dpZD1NVFkzTkRJNE16RTNOek0xTWpZM05EUXlO
-	#RGc+DQo=
-	#--000000000000ab91de0614407291
+	while ($base64 =~ /Content-Type: (text\/plain|text\/html);((.*?\n?.*?){0,3})(Encoding: base64)((.*?\n?.*?)+)=/g){
+		my $type = $1;
+		my $b64ent = $5;
+		#print color("red"), "ausgabe: ", color("reset"), "$5 ", pos $base64, "\n";
+		$b64ent = MIME::Base64::decode($b64ent);
+		$b64ent =~ s|<.+?>||g;                      # html raus (manche links können von interesse sein)           
+		$b64ent =~ s/^ +//gm ;						# remove whitespaces at start of line
+		$b64ent =~ s/\n\s*/\n/g;					# remove empty lines
+		print color("red"), "Base64(", color("white"), "$type", color("red"), ") DECODE: ", color("yellow"), "$b64ent\n", color("reset");
+	}
 }
 
 ### END
