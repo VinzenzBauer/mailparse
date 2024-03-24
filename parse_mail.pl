4d3818c040bf98db82c3a62650747e536fb399aa
vinzenzbauer <vinzenz.bauer@gmx.de>
2024-03-24 21:53:17 +0100

    kuerzer

--- parse_mail.pl
+++ parse_mail.pl
@@ -24,7 +24,7 @@ my $content = "";
 
 my $bounce = 0;
 
-### FUNCTIONS
+### SUBROUTINES
 sub remove_strings {
     my ($input, $rem) = @_;
     my @rem = @{ $rem };
@@ -42,85 +42,15 @@ sub decode_iso_8859_1_base64 {		# bsp 5: subject from etc waren base64 mit iso-t
 }
 sub decode_guess{
 	my $line = shift;
-	if ($line =~ m/=\?iso/) {
+	if ($line =~ m/=\?iso-8859/) {
 		return decode_iso_8859_1_base64("$line");
 	};
 	return $line;
 }
-
-### READ WHOLE FILE
-foreach my $line ( <STDIN> ) {
-    #chomp( $line );
-    $PIPE .= $line;
-}
-
-### SEPARATE HEAD AND BODY
-my ($head, $body) = split /\n\h*\n/, $PIPE, 2;
-
-### MAIL HEAD
-#print color("red"), "$head\n", color("reset");
-my @headA = split (/\n/, $head);
-foreach my $line (@headA) {
-	#print "$line\n";
-	if ($line =~ m/^$M0/) {
-		$line =~ s/^[^=]*=//;
-		printf "%s:\t\t%s ", "sasl", $line;
-	};
-	if ($line =~ m/$IP/) {
-		$line =~ s/^[^=]*=//;
-		printf "<%s>\n", $line;
-	};
-	if ($line =~ m/^$DATE/) {
-		$line =~ s/^[^,]*,\s//;
-		printf "%s:\t\t%s\n", "date", $line;
-	};
-	if ($line =~ m/^$FROM/) {
-		$line =~ s/^[^:]*:\s//;
-		$line = decode_guess("$line");
-		printf "%s:\t\t%s\n", "from", $line;
-	};
-	if ($line =~ m/^$TO/) {
-		$line =~ s/^[^:]*:\s//;
-		$line = decode_guess("$line");
-		printf "%s:\t\t%s\n", "to", $line;
-	};
-	if ($line =~ m/^$REPLYTO/) {
-		$line =~ s/^[^:]*:\s//;
-		$line = decode_guess("$line");
-		printf "%s:\t%s\n", "replyto", $line;
-	};
-	if ($line =~ m/^$SUBJECT/) {
-		$line =~ s/^[^:]*:\s//;
-		$line = decode_guess("$line");
-		printf "%s:\t%s\n", "subject", $line;
-	};
-}
-
-### MAIL BODY
-#print color("green"), "$body\n", color("reset");
-
-## HTML CONTENT (bsp: 1, 3, 7)
-$content = $body; 
-$content =~ s/\R//g;							# remove linebreaks
-$content =~ s/.*?$htmlstart(.*)$htmlend.*/$1/s;	# extract content between html-body-tags
-$content =~ s|<.+?>||g;                         # html raus
-$content =~ s/{.+}//sg;                         # webkit raus
-$content = remove_strings($content, \@removes);	# spezielle tags raus
-$content =~ s/^ +//gm ;							# remove whitespaces at start of line
-$content =~ s/\n\s*/\n/g;						# remove empty lines
-$content =~ s/\h+/ /g;                        	# replace multispaces with single space
-
-## ERROR EG.: Content-Description: Undelivered Message
-#Reply-To: WinBTC <bitcoin-mining@groupalim.com>
-#From: WinBTC <benjamin@widmann-elektrotechnik.de>
-#Subject: Balance Mining BTC 
-#Date: Fri, 22 Mar 2024 16:29:30 +0000
-my @bodyA = split (/\n/, $body);
-if ($body =~ /I'm sorry to have to inform you that your message could not/s) {
-	print '-' x 80, "\n"; 
-	$bounce = 1;
-	
-	foreach my $line (@bodyA) {
+sub printHeaderInfo{
+	my $head = shift;
+	my @headA = split (/\n/, $head);
+	foreach my $line (@headA) {
 		if ($line =~ m/^$M0/) {
 			$line =~ s/^[^=]*=//;
 			printf "%s:\t\t%s ", "sasl", $line;
@@ -135,21 +65,61 @@ if ($body =~ /I'm sorry to have to inform you that your message could not/s) {
 		};
 		if ($line =~ m/^$FROM/) {
 			$line =~ s/^[^:]*:\s//;
+			$line = decode_guess("$line");
 			printf "%s:\t\t%s\n", "from", $line;
 		};
 		if ($line =~ m/^$TO/) {
 			$line =~ s/^[^:]*:\s//;
+			$line = decode_guess("$line");
 			printf "%s:\t\t%s\n", "to", $line;
 		};
 		if ($line =~ m/^$REPLYTO/) {
 			$line =~ s/^[^:]*:\s//;
+			$line = decode_guess("$line");
 			printf "%s:\t%s\n", "replyto", $line;
 		};
 		if ($line =~ m/^$SUBJECT/) {
 			$line =~ s/^[^:]*:\s//;
+			$line = decode_guess("$line");
 			printf "%s:\t%s\n", "subject", $line;
 		};
 	}
+}
+
+### READ WHOLE FILE
+foreach my $line ( <STDIN> ) {
+    $PIPE .= $line;
+}
+
+### SEPARATE HEAD AND BODY
+my ($head, $body) = split /\n\h*\n/, $PIPE, 2;
+
+### MAIL HEAD
+printHeaderInfo($head);
+
+### MAIL BODY
+## HTML CONTENT (bsp: 1, 3, 7)
+$content = $body; 
+$content =~ s/\R//g;							# remove linebreaks
+$content =~ s/.*?$htmlstart(.*)$htmlend.*/$1/s;	# extract content between html-body-tags
+$content =~ s|<.+?>||g;                         # html raus
+$content =~ s/{.+}//sg;                         # webkit raus
+$content = remove_strings($content, \@removes);	# spezielle tags raus
+$content =~ s/^ +//gm ;							# remove whitespaces at start of line
+$content =~ s/\n\s*/\n/g;						# remove empty lines
+$content =~ s/\h+/ /g;                        	# replace multispaces with single space
+
+if ($body =~ /I'm sorry to have to inform you that your message could not/s) {
+
+	## ERROR EG.: Content-Description: Undelivered Message
+	#Reply-To: WinBTC <bitcoin-mining@groupalim.com>
+	#From: WinBTC <benjamin@widmann-elektrotechnik.de>
+	#Subject: Balance Mining BTC 
+	#Date: Fri, 22 Mar 2024 16:29:30 +0000
+	print '-' x 80, "\n"; 
+	$bounce = 1;
+	printHeaderInfo($body);
+	
 	# bsp: 10 alles nach X-Spamd-Bar: von interesse: Hallo * * * 馃獧 袙袗袦 袟袗效袠小袥袝袧袨 ...
 	if ($body =~ /X-Spamd-Bar: \/\n/s) {
 		$content = $body;
